8c467cb67ce7d4c3ead46d57cc629981e15142e5
Daniel <danielstarling4@gmail.com>
2019-05-31 10:27:58 -0700

    updating comments

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,32 +1,22 @@
 import csv
-# Create a class to hold a city location. Call the class "City". It should have
-# fields for name, lat and lon (representing latitude and longitude).
-
-# We have a collection of US cities with population over 750,000 stored in the
-# file "cities.csv". (CSV stands for "comma-separated values".)
-#
-# In the body of the `cityreader` function, use Python's built-in "csv" module
-# to read this file so that each record is imported into a City instance. Then
-# return the list with all the City instances from the function.
-# Google "python 3 csv" for references and use your Google-fu for other examples.
-#
-# Store the instances in the "cities" list, below.
-#
-# Note that the first line of the CSV is header that describes the fields--this
-# should not be loaded into a City object.
 
 
+# Create City class with name lat and lon
 class City:
     def __init__(self, name, lat, lon):
         self.name = name
         self.lat = lat
         self.lon = lon
+    # return name, lat and lon
 
     def __repr__(self):
         return f'{self.name}: {self.lat}, {self.lon}'
 
 
+# List to push cities too as they are parsed by cityreader function
 cities = []
+
+# read cities from CSV doc.
 
 
 def cityreader(cities=[]):
@@ -34,6 +24,7 @@ def cityreader(cities=[]):
         csvreader = csv.reader(csvfile)
         next(csvreader)
         for row in csvreader:
+            # Change the numbers into floats
             new_city = City(row[0], float(row[3]), float(row[4]))
             cities.append(new_city)
     return cities
@@ -43,6 +34,7 @@ cityreader(cities)
 # Print the list of cities (name, lat, lon), 1 record per line.
 for city in cities:
     print(city)
+
 
 # STRETCH GOAL!
 #
@@ -85,6 +77,3 @@ def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
     # the specified coordinates.
 
     return within
-
-# Create a class to hold a city location. Call the class "City". It should have
-# fields for name, latitude, and longitude.
